2f3378fefbdc6ea57baf65ba2145b1eadedee8ad
nvalerio12 <nelson.j.valerio@gmail.com>
2021-03-03 15:27:36 -0500

    added all files

---
+++ models/example.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const exampleSchema = new Schema({
+    name: String,
+    completed: Boolean,
+});
+
+const Example = mongoose.model('Example', exampleSchema);
+
+module.exports = Example;
+
